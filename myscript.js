f799ec6c658a307a646e0da6a7ca747127ea3395
leitongchen <leitongchen93@gmail.com>
2021-04-08 15:52:32 +0200

    prezzo in forma umana

--- myscript.js
+++ myscript.js
@@ -4,21 +4,21 @@ var userAge = parseInt(prompt("Indica la tua età attuale"));
 
 var costPerKm = 0.21; 
 
-console.log(nrKm, userAge);
+console.log("Devi percorrere " + nrKm + " km.");
+console.log("Hai " + userAge + " anni.")
 
 var costTicket = nrKm * costPerKm; 
 
 var promoYoung = costTicket * 20 / 100;
-
 var promoSenior = costTicket * 40 / 100;
 
 
 if (userAge < 18 ) {
-    console.log("Il costo del tuo biglietto è di €" + (costTicket - promoYoung));
+    console.log("Il costo del tuo biglietto è di €" + (costTicket - promoYoung).toFixed(2));
 
 } else if (userAge >= 65) {
-    console.log("Il costo del tuo biglietto è di €" + (costTicket - promoSenior));
+    console.log("Il costo del tuo biglietto è di €" + (costTicket - promoSenior).toFixed(2));
 
 } else {
-    console.log("Il costo del tuo biglietto è di €" + costTicket);
+    console.log("Il costo del tuo biglietto è di €" + costTicket.toFixed(2));
 }
