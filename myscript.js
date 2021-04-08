4b62f303fff86e8a21f307d7b071c287591b4a57
leitongchen <leitongchen93@gmail.com>
2021-04-08 19:12:13 +0200

    iniettato testo js nell'html

--- myscript.js
+++ myscript.js
@@ -1,6 +1,7 @@
 var nrKm = parseInt(prompt("Indica il numero di km che devi percorrere, a partire da 5km"));
 
 if (Number.isNaN(nrKm) || nrKm < 5) {
+    
     alert("Si prega di inserire un numero di km valido, a partire da 5km.")
 
 } else {
@@ -8,16 +9,17 @@ if (Number.isNaN(nrKm) || nrKm < 5) {
     var userAge = parseInt(prompt("Indica la tua età attuale"));
 
     if (Number.isNaN(userAge)) {
+
         alert("Il dato inserito deve essere un numero");
     
     } else if (userAge < 0 || userAge > 119) {
+
         alert("L'età che hai inserito non è valida.")
 
     }  else {
         
-        console.log("Devi percorrere " + nrKm + " km.");
-        console.log("Hai " + userAge + " anni.");
-    
+        document.getElementById("user-info").innerHTML="Devi percorrere " + nrKm + " km e hai"  + userAge + " anni."
+
         var costPerKm = 0.21; 
         var costTicket = nrKm * costPerKm; 
     
@@ -25,13 +27,22 @@ if (Number.isNaN(nrKm) || nrKm < 5) {
         var promoSenior = costTicket * 40 / 100; 
     
         if (userAge < 18 ) {
-            console.log("Con uno sconto già applicato del 20%, il costo del tuo biglietto è di €" + (costTicket - promoYoung).toFixed(2));
+
+            document.getElementById("ticket-cost").innerHTML="Con uno sconto già applicato del 20%, il costo del tuo biglietto è di €" + (costTicket - promoYoung).toFixed(2);
+
+            //console.log("Con uno sconto già applicato del 20%, il costo del tuo biglietto è di €" + (costTicket - promoYoung).toFixed(2));
     
         } else if (userAge >= 65) {
-            console.log("Con uno sconto già applicato del 40%, il costo del tuo biglietto è di €" + (costTicket - promoSenior).toFixed(2));
+            
+            document.getElementById("ticket-cost").innerHTML="Con uno sconto già applicato del 40%, il costo del tuo biglietto è di €" + (costTicket - promoSenior).toFixed(2); 
+
+            //console.log("Con uno sconto già applicato del 40%, il costo del tuo biglietto è di €" + (costTicket - promoSenior).toFixed(2));
     
         } else {
-            console.log("Il costo del tuo biglietto è di €" + costTicket.toFixed(2));
+
+            document.getElementById("ticket-cost").innerHTML="Il costo del tuo biglietto è di €" + costTicket.toFixed(2)
+
+            //console.log("Il costo del tuo biglietto è di €" + costTicket.toFixed(2));
         }
     } 
 
